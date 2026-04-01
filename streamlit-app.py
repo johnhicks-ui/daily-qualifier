ea8d1cc7ebeb80a737a4497080afddd36f3401c1
johnhicks-ui <john_hicks@sky.com>
2026-04-01 14:15:36 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,17 +1,5 @@
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
 
-def test_open_race(url):
-    import requests
-    from bs4 import BeautifulSoup
+st.title("Daily Qualifier")
 
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-
-    r = requests.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    # show us raw page text so we can find horses next
-    return soup.get_text()[:1000]
+st.write("App is running correctly")
