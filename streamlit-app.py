f3256c00e2b0365d2ca66b7a63ac287b43c9ef53
johnhicks-ui <john_hicks@sky.com>
2026-04-01 14:21:21 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import requests
 
 st.title("Daily Qualifier")
 
-st.write("App is running correctly")
+def test_connection():
+    url = "https://www.racingpost.com/racecards/"
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    r = requests.get(url, headers=headers)
+
+    return r.status_code
+
+st.write("Status code:", test_connection())
