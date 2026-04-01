df4d2d25f0872d87352eb7d6a799108de75f4431
johnhicks-ui <john_hicks@sky.com>
2026-04-01 13:56:09 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -9,16 +9,13 @@ st.title("Daily Qualifier - Test Build")
 # -----------------------------
 def test_open_race(url):
     headers = {
-        "User-Agent": "Mozilla/5.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
     }
 
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, "html.parser")
 
-    if soup.title:
-        return soup.title.text
-    else:
-        return "No title found"
+    return soup.get_text()[:300]
 
 # -----------------------------
 # RUN TEST
