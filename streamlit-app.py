9dd148baec9b69cc18322f7dc96e1ff4cc959275
johnhicks-ui <john_hicks@sky.com>
2026-04-05 15:04:47 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,34 +2,7 @@ import streamlit as st
 
 st.write("NEW CODE IS RUNNING")
 
-def get_runners(url):
-    import requests
-    from bs4 import BeautifulSoup
 
-    headers = {"User-Agent": "Mozilla/5.0"}
-    r = requests.get(url, headers=headers)
-
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    horses = []
-
-    for td in soup.find_all("td"):
-        text = td.get_text(strip=True)
-
-        if text and text[0].isdigit() and "." in text:
-            parts = text.split(".")
-            if len(parts) > 1:
-                name = parts[1].split("(")[0].strip()
-
-                if len(name) > 2:
-                    horses.append(name)
-
-    horses = list(set(horses))
-
-    if not horses:
-        return None
-
-    return horses[0]
 
 
 
@@ -43,6 +16,7 @@ def get_racecards():
         "https://www.racingpost.com/racecards/177/clonmel/2026-04-02/916749",
         "https://www.racingpost.com/racecards/205/auteuil/2026-04-02/917017",
         "https://www.racingpost.com/racecards/27/kelso/2026-04-02/914327"
+    ]
 st.title("Daily Qualifier (API Build)")
 
 links = get_racecards()
