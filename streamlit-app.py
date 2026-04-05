8b98278a85723fd7fbcaaeb3f1d2033f55ae421e
johnhicks-ui <john_hicks@sky.com>
2026-04-05 14:52:21 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -38,7 +38,11 @@ def get_runners(url):
 # DEBUG LOOP
 # -----------------------------
 
-
+def get_racecards():
+    return [
+        "https://www.racingpost.com/racecards/177/clonmel/2026-04-02/916749",
+        "https://www.racingpost.com/racecards/205/auteuil/2026-04-02/917017",
+        "https://www.racingpost.com/racecards/27/kelso/2026-04-02/914327"
 st.title("Daily Qualifier (API Build)")
 
 links = get_racecards()
