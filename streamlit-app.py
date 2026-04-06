f91f840b6d98d9c6fb3a6c33cf44002cfa4a9a56
johnhicks-ui <john_hicks@sky.com>
2026-04-06 14:24:41 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,18 +1,21 @@
 import streamlit as st
 import requests
+import re
+import json
 from bs4 import BeautifulSoup
 
-st.write("NEW CODE IS RUNNING")
+st.title("Daily Qualifier (API Build)")
 
 
 def get_racecards():
-import re
-import json
+    return [
+        "https://www.racingpost.com/racecards/177/clonmel/2026-04-02/916749",
+        "https://www.racingpost.com/racecards/205/auteuil/2026-04-02/917017",
+        "https://www.racingpost.com/racecards/27/kelso/2026-04-02/914327"
+    ]
+
 
 def get_runners(url):
-    import re
-    import json
-
     headers = {"User-Agent": "Mozilla/5.0"}
     r = requests.get(url, headers=headers)
 
@@ -44,16 +47,7 @@ def get_runners(url):
                 continue
 
     return None
-        "https://www.racingpost.com/racecards/177/clonmel/2026-04-02/916749",
-        "https://www.racingpost.com/racecards/205/auteuil/2026-04-02/917017",
-        "https://www.racingpost.com/racecards/27/kelso/2026-04-02/914327"
-    ]
 
-
-
-
-
-st.title("Daily Qualifier (API Build)")
 
 links = get_racecards()
 
@@ -61,5 +55,6 @@ st.write("Race Links:")
 st.write(links)
 
 st.write("Qualifier from first race:")
+
 if links:
     st.write(get_runners(links[0]))
